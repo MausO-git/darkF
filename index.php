e2a85e68874113bc03227e2d508787dc645ac8b1
MausO <oscarmaus01@gmail.com>
2025-05-01 18:26:57 +0200

    fixed nav

--- index.php
+++ index.php
@@ -4,10 +4,10 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>DF shop</title>
-    <link rel="stylesheet" href="style.css" type="text/css">
+    <link rel="stylesheet" href="build/css/style.css" type="text/css">
 </head>
 <body>
-    <?php
+<?php
         include("partials/header.php")
     ?>
 </body>
--- partials/header.php
+++ partials/header.php
@@ -1,8 +1,10 @@
 <header>
     <nav>
-        <ul><li><a href="#">Menu 1</a></li></ul>
-        <ul><li><a href="#">Menu 2</a></li></ul>
-        <ul><li><a href="#">Menu 3</a></li></ul>
-        <ul><li><a href="#">Menu 4</a></li></ul>
+        <ul>
+            <li><a href="#">Menu 1</a></li>
+            <li><a href="#">Menu 2</a></li>
+            <li><a href="#">Menu 3</a></li>
+            <li><a href="#">Menu 4</a></li>
+        </ul>
     </nav>
 </header>
