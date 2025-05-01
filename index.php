00e194de71c92baf0a5fd926f6568e0e35aa1f25
MausO <oscarmaus01@gmail.com>
2025-05-01 18:00:38 +0200

    second

--- index.php
+++ index.php
@@ -4,9 +4,11 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>DF shop</title>
-    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="style.css" type="text/css">
 </head>
 <body>
-    
+    <?php
+        include("partials/header.php")
+    ?>
 </body>
 </html>
