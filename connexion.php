f167ef67de7344c851e2f259a8f2fe2533aeecc1
MausO <oscarmaus01@gmail.com>
2025-05-02 15:49:19 +0200

    lien bdd

---
+++ connexion.php
@@ -0,0 +1,7 @@
+<?php
+try{
+    $bdd=new PDO('mysql:host=localhost;dbname=darkf;charset=utf8','root','',[PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION]);
+}catch(Exception $e){
+    die("Erreur: ".$e->getMessage());
+};
+?>
--- index.php
+++ index.php
@@ -8,7 +8,28 @@
 </head>
 <body>
     <?php
-        include("partials/header.php")
+        include("partials/header.php");
     ?>
+    <div class="wrapper">
+        <h1>Liste des articles en stock</h1>
+        <div id="pres">
+            <?php
+                require("connexion.php");
+
+                $req = $bdd->query("SELECT * FROM art");
+                
+                while($don = $req->fetch(PDO::FETCH_ASSOC)){
+                    //var_dump($don);
+                    echo "<a href='art.php?".$don['id']."' class='art'>";
+                        echo "<h1>".$don['nom']."</h1>";
+                        echo "<img src='images/".$don['media']."' alt='images'";
+                    echo "</a>";
+                };
+                //
+                $req->closeCursor();
+                
+            ?>
+        </div>
+    </div>
 </body>
 </html>
